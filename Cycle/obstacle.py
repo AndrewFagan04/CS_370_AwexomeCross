11d043f94d0d8af071a89679dc26894c282fb1f0
Mian <danyalm@sunypoly.edu>
2024-09-30 15:40:41 -0400

    Invincibility works, speed, background and meteors need to move too so not done

--- Cycle/obstacle.py
+++ Cycle/obstacle.py
@@ -5,15 +5,16 @@ class Obstacle(pygame.sprite.Sprite):
     def __init__(self, obstacle_speed, WIDTH, LENGTH):
         super().__init__()
         self.image = pygame.image.load("C:/Users/Danyal/CS_370_danyalm/CS_370_AwexomeCross/sprites/Meteor_06.png")
-        self.image = pygame.transform.scale(self.image, (100, 100))  # Ensure the image is scaled to 100x100
+        self.image = pygame.transform.scale(self.image, (100, 100))  #change how big it is
 
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, WIDTH - self.rect.width)  # Ensure valid range
+        self.rect.x = random.randint(0, WIDTH - self.rect.width) #spawn in range
         self.rect.y = -self.rect.height
-        self.speed = obstacle_speed  # Store the speed
-        self.LENGTH = LENGTH  # Store the length of the game window
+        self.speed = obstacle_speed  # speed
+        self.LENGTH = LENGTH  # game window length, so i can use in the bottom function
 
     def update(self):
-        self.rect.y += self.speed  # Use the stored speed
-        if self.rect.y > self.LENGTH:  # Use the stored length
+        #makes it move down at the speed
+        self.rect.y += self.speed  
+        if self.rect.y > self.LENGTH:
             self.kill()
