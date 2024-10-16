a1ef204c9fb1ce564585648006a7f24a46065813
Kevin <wangk@sunypoly.edu>
2024-10-16 14:14:54 -0400

    Created audio folder for audio files in cycle 2, renamed filepath to direct to current folders

--- Cycle_2/obstacle.py
+++ Cycle_2/obstacle.py
@@ -5,7 +5,7 @@ from os.path import join
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self, obstacle_speed, WIDTH, LENGTH):
         super().__init__()
-        self.image = pygame.image.load(join('Cycle\sprites',"Meteor_06.png"))
+        self.image = pygame.image.load(join('Cycle_2\sprites',"Meteor_06.png"))
         self.image = pygame.transform.scale(self.image, (100, 100))  # Ensure the image is scaled to 100x100
 
         self.rect = self.image.get_rect()
--- Cycle_2/powerups.py
+++ Cycle_2/powerups.py
@@ -7,7 +7,7 @@ class Invincibility(pygame.sprite.Sprite):
     def __init__(self, obstacle_speed, WIDTH, LENGTH):
         super().__init__()
         #self.image = pygame.image.load("C:/Users/Danyal/CS_370_danyalm/CS_370_AwexomeCross/Cycle/sprites/coin.png")
-        self.image = pygame.image.load(join('Cycle\sprites',"goldcoin22.png"))
+        self.image = pygame.image.load(join('Cycle_2\sprites',"goldcoin22.png"))
 
         self.image = pygame.transform.scale(self.image, (150, 90)) 
         self.rect = self.image.get_rect()
